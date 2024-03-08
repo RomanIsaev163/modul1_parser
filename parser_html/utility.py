2db8362e65ad24fafabe2ea2ee3a6072ec1d2c4f
RomanIsaev <mr.romka.01@mail.ru>
2024-03-08 21:39:02 +0300

    add try except return

--- parser_html/utility.py
+++ parser_html/utility.py
@@ -2,5 +2,9 @@ import requests as rq
 from fake_useragent import UserAgent
 
 
-def get_html(url: str) -> bytes:
-    return rq.get(url, headers={'User-Agent': UserAgent().chrome}, timeout=15)
+def get_html(url: str) -> rq.models.Response:
+    try:
+        response = rq.get(url, headers={'User-Agent': UserAgent().chrome}, timeout=15)
+        return response
+    except:
+        return None
